baf96e7f41f23966ecec7180c28d78e8394650e4
d-sar <damrisara1@gmail.com>
2025-03-10 19:33:15 +0000

    creeation de projet mvn avec les depandences

---
+++ src/main/java/ma/enset/gestion_des_patients/GestionDesPatientsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.gestion_des_patients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesPatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesPatientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/gestion_des_patients/GestionDesPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.gestion_des_patients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesPatientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
