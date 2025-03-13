2b2f31ec6b17ce2a8c8bc9c48631f37b1a44dd9f
d-sar <damrisara1@gmail.com>
2025-03-13 22:04:41 +0000

    mini service web

--- src/main/java/ma/enset/gestion_des_patients/web/PatientRestService.java
+++ src/main/java/ma/enset/gestion_des_patients/web/PatientRestService.java
@@ -1,4 +1,28 @@
 package ma.enset.gestion_des_patients.web;
 
+import jakarta.persistence.Access;
+import ma.enset.gestion_des_patients.entities.Patient;
+import ma.enset.gestion_des_patients.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PatientRestService {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @GetMapping("/patients")
+    public List<Patient> patients(){
+        return patientRepository.findAll();
+    }
+    @GetMapping("/patients/{id}")
+    public Patient findPatient(@PathVariable Long id){
+        Patient patient = patientRepository.findById(id).orElse(null);// return un objet optionnel
+        return patient;
+    }
+
 }
